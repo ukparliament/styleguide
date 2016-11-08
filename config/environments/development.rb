eef19491c23ded1acdafc030e0972e359c86678f
Peter Hunt <huntpb@parliament.uk>
2016-11-08 16:06:43 +0000

    added config to beautify slim output

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Beautify html for debugging and do sort attributes
+  Slim::Engine.set_options pretty: true, sort_attrs: true
 end
