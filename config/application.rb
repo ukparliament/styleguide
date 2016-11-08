51b41701d2cdce2f0462a4c043ed588e444f897b
Peter Hunt <huntpb@parliament.uk>
2016-11-08 16:06:02 +0000

    enabled slim templating

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+# Required for slim and slim includes
+require 'slim'
+require 'slim/include'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
