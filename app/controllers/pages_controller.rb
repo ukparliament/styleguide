40f249c1ca17db762984d07fdcf91cc0fe3142a9
vinternet-pds <vinterj@parliament.uk>
2016-11-14 15:49:24 +0000

    Rails controller & routing updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
   protect_from_forgery with: :exception
-
-  def dev
-    render layout: "application"
+  def design
     # render "{pathOfViewsFile}" layout: "{layoutTemplate}"
+  end
+  def elements
+    render "pages/elements/typography"
+  end
+  def components
+    render "pages/components/header"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  # Add Application helper
 end
