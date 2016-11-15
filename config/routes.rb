430893487a4392628539b3bbbb1f4c57c5c52a91
vinternet-pds <vinterj@parliament.uk>
2016-11-15 11:06:26 +0000

    Ruby controllers update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: 'styleguide#index'
 
   # get '/components' , to: 'pages#components', as: 'components'
-
   get '/components' => redirect('/components/header', status: 302)
+  get '/design' => redirect('/design/colours', status: 302)
   get '/elements' => redirect('/elements/typography', status: 302)
 end
