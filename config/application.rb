18af46a104e4ce7242d0beb62b469ee719d48967
Jake Mulley <mulleyjb@parliament.uk>
2016-11-16 11:19:45 +0000

    fix(precompilation): turn off

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Styleguide
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
