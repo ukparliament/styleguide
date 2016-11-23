af7bb5bf74085de636b8e11326df0527e208f55b
Peter Hunt <huntpb@parliament.uk>
2016-11-23 15:40:01 +0000

    added Rspec test for style guide controllers

---
+++ spec/controllers/templates_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe TemplatesController, type: :controller do
+
+  describe "GET #member_profile_hoc" do
+    before(:each) do
+      get :member_profile_hoc
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:success) # :error :missing :redirect :success
+    end
+    it "rendered using the style guide template" do
+      expect(response).to render_template("styleguide/templates/member_profile_hoc")
+    end
+  end
+
+  describe "GET #member_profile_hol" do
+    before(:each) do
+      get :member_profile_hol
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:success) # :error :missing :redirect :success
+    end
+    it "rendered using the style guide template" do
+      expect(response).to render_template("styleguide/templates/member_profile_hol")
+    end
+  end
+
+end
--- spec/views/styleguide/design/colours.html.slim_spec.rb
+++ spec/views/styleguide/design/colours.html.slim_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "styleguide/index.html.slim", type: :view do
+RSpec.describe "styleguide/design/colours.html.slim", type: :view do
 
-  describe "GET #index" do
+  describe "GET #colours" do
     before(:each) do
       # you'll need to specify both the template and the layout explicitly
-      render template: 'styleguide/index', layout: 'layouts/sub_layout'
+      render template: 'styleguide/design/colours', layout: 'layouts/sub_layout'
     end
     # Repeat for each partial
     it "rendered partial" do
       expect(response.body).to render_template(partial: "_nav_hr")
     end
     it "rendered view includes '#WordorPhrase'" do
-      render
-      expect(response.body).to include('UK Parliament Style Guide')
+      expect(response.body).to include("Colours")
     end
   end
 
