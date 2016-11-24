7d5d73f63941861f2becd2aabf18b9ead3090643
Peter Hunt <huntpb@parliament.uk>
2016-11-24 16:57:42 +0000

    STYLE-12, Add unit tests for exisiting styleguide pages.

--- spec/views/styleguide/components/header.html.slim_spec.rb
+++ spec/views/styleguide/components/header.html.slim_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "styleguide/design/colours.html.slim", type: :view do
+RSpec.describe "styleguide/components/header.html.slim", type: :view do
 
-  describe "GET #colours" do
+  describe "GET #header" do
     before(:each) do
       # you'll need to specify both the template and the layout explicitly
-      render template: 'styleguide/design/colours', layout: 'layouts/sub_layout'
+      render template: 'styleguide/components/breadcrumb', layout: 'layouts/sub_layout'
     end
     # Repeat for each partial
     it "rendered partial" do
-      expect(response.body).to render_template(partial: "_colours")
+      expect(response.body).to render_template(partial: "_header")
     end
     it "rendered partial" do
       expect(response.body).to render_template(partial: "_nav_hr")
     end
     it "rendered partial" do
-      expect(response.body).to render_template(partial: "_nav_vr_design")
+      expect(response.body).to render_template(partial: "_nav_vr_components")
     end
     it "rendered view includes '#WordorPhrase'" do
-      expect(response.body).to include("Colours")
+      expect(response.body).to include("Header")
     end
   end
 
--- spec/views/styleguide/design/typography.html.slim_spec.rb
+++ spec/views/styleguide/design/typography.html.slim_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "styleguide/design/colours.html.slim", type: :view do
+RSpec.describe "styleguide/design/typography.html.slim", type: :view do
 
-  describe "GET #colours" do
+  describe "GET #typography" do
     before(:each) do
       # you'll need to specify both the template and the layout explicitly
-      render template: 'styleguide/design/colours', layout: 'layouts/sub_layout'
+      render template: 'styleguide/design/typography', layout: 'layouts/sub_layout'
     end
     # Repeat for each partial
     it "rendered partial" do
-      expect(response.body).to render_template(partial: "_colours")
+      expect(response.body).to render_template(partial: "_typography")
     end
     it "rendered partial" do
       expect(response.body).to render_template(partial: "_nav_hr")
@@ -18,7 +18,7 @@ RSpec.describe "styleguide/design/colours.html.slim", type: :view do
       expect(response.body).to render_template(partial: "_nav_vr_design")
     end
     it "rendered view includes '#WordorPhrase'" do
-      expect(response.body).to include("Colours")
+      expect(response.body).to include("Typography")
     end
   end
 
