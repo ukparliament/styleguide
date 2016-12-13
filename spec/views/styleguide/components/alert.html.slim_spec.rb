a03259ce23af03f6821bcf2c9bf9a1b0488b33ee
Peter Hunt <naseberry@users.noreply.github.com>
2016-12-13 14:22:35 +0000

    STYLE-38, Rspec: Alerts view

---
+++ spec/views/styleguide/components/alert.html.slim_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "styleguide/components/alert.html.slim", type: :view do
+
+  describe "GET #a-z" do
+    before(:each) do
+      # you'll need to specify both the template and the layout explicitly
+      render template: 'styleguide/components/alert', layout: 'layouts/sub_layout'
+    end
+
+    it "rendered layout partials" do
+      expect(response.body).to render_template(partial: "_header")
+      expect(response.body).to render_template(partial: "_nav_hr")
+      expect(response.body).to render_template(partial: "_nav_vr_components")
+      expect(response.body).to render_template(partial: "_footer")
+    end
+
+    it "rendered page partials" do
+      expect(response.body).to render_template(partial: "_standard")
+      expect(response.body).to render_template(partial: "_information")
+      expect(response.body).to render_template(partial: "_success")
+      expect(response.body).to render_template(partial: "_warning")
+      expect(response.body).to render_template(partial: "_error")
+    end
+
+    it "rendered view includes '#WordorPhrase'" do
+      expect(response.body).to include("Alert")
+    end
+  end
+
+end
