c1bf9f7573d5941771fe9c38215ea5ad21a13782
Peter Hunt <naseberry@users.noreply.github.com>
2016-12-13 14:24:40 +0000

    updated controller specs

--- spec/controllers/elements_controller_spec.rb
+++ spec/controllers/elements_controller_spec.rb
@@ -2,26 +2,50 @@ require 'rails_helper'
 
 RSpec.describe ElementsController, type: :controller do
 
-  describe "GET #lists" do
+  describe "GET forms" do
+    before(:each) do
+      get :forms
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+    it "rendered using the correct template" do
+      expect(response).to render_template("styleguide/elements/forms")
+    end
+  end
+
+  describe "GET lists" do
     before(:each) do
       get :lists
     end
     it "returns http success" do
-      expect(response).to have_http_status(:success) # :error :missing :redirect :success
+      expect(response).to have_http_status(:success)
     end
-    it "rendered using the style guide template" do
+    it "rendered using the correct template" do
       expect(response).to render_template("styleguide/elements/lists")
     end
   end
 
-  describe "GET #table" do
+  describe "GET media" do
+    before(:each) do
+      get :media
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+    it "rendered using the correct template" do
+      expect(response).to render_template("styleguide/elements/media")
+    end
+  end
+
+  describe "GET table" do
     before(:each) do
       get :table
     end
     it "returns http success" do
-      expect(response).to have_http_status(:success) # :error :missing :redirect :success
+      expect(response).to have_http_status(:success)
     end
-    it "rendered using the style guide template" do
+    it "rendered using the correct template" do
       expect(response).to render_template("styleguide/elements/table")
     end
   end
--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe TemplatesController, type: :controller do
 
-  describe "GET #member_profile_hoc" do
+  describe "GET member_profile_hoc" do
     before(:each) do
       get :member_profile_hoc
     end
     it "returns http success" do
-      expect(response).to have_http_status(:success) # :error :missing :redirect :success
+      expect(response).to have_http_status(:success)
     end
-    it "rendered using the style guide template" do
+    it "rendered using the correct template" do
       expect(response).to render_template("styleguide/templates/member_profile_hoc")
     end
   end
 
-  describe "GET #member_profile_hol" do
+  describe "GET member_profile_hol" do
     before(:each) do
       get :member_profile_hol
     end
     it "returns http success" do
-      expect(response).to have_http_status(:success) # :error :missing :redirect :success
+      expect(response).to have_http_status(:success)
     end
-    it "rendered using the style guide template" do
+    it "rendered using the correct template" do
       expect(response).to render_template("styleguide/templates/member_profile_hol")
     end
   end
