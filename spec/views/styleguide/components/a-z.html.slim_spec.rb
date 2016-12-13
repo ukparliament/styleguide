ebb5f2a722835ce9837a30a58af266329325e908
Peter Hunt <naseberry@users.noreply.github.com>
2016-12-13 14:20:56 +0000

    STYLE-35, Rspec: A-Z view

---
+++ spec/views/styleguide/components/a-z.html.slim_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "styleguide/components/a-z.html.slim", type: :view do
+
+  describe "GET #a-z" do
+    before(:each) do
+      # you'll need to specify both the template and the layout explicitly
+      render template: 'styleguide/components/a-z', layout: 'layouts/sub_layout'
+    end
+
+    it "rendered layout partials" do
+      expect(response.body).to render_template(partial: "_header")
+      expect(response.body).to render_template(partial: "_nav_hr")
+      expect(response.body).to render_template(partial: "_nav_vr_components")
+      expect(response.body).to render_template(partial: "_footer")
+    end
+
+    it "rendered page partials" do
+      expect(response.body).to render_template(partial: "_a-z")
+    end
+
+    it "rendered view includes '#WordorPhrase'" do
+      expect(response.body).to include("A - Z")
+    end
+  end
+
+end
