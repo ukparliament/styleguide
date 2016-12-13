4f13dbc1f190c5301f4cb950512cde1e2eb78d05
Peter Hunt <naseberry@users.noreply.github.com>
2016-12-13 14:21:22 +0000

    STYLE-36, Rspec: Badges view

---
+++ spec/views/styleguide/components/badges.html.slim_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "styleguide/components/badges.html.slim", type: :view do
+
+  describe "GET #a-z" do
+    before(:each) do
+      # you'll need to specify both the template and the layout explicitly
+      render template: 'styleguide/components/badges', layout: 'layouts/sub_layout'
+    end
+
+    it "rendered layout partials" do
+      expect(response.body).to render_template(partial: "_header")
+      expect(response.body).to render_template(partial: "_nav_hr")
+      expect(response.body).to render_template(partial: "_nav_vr_components")
+      expect(response.body).to render_template(partial: "_footer")
+    end
+
+    it "rendered page partials" do
+      expect(response.body).to render_template(partial: "_small")
+      expect(response.body).to render_template(partial: "_standard")
+      expect(response.body).to render_template(partial: "_large")
+    end
+
+    it "rendered view includes '#WordorPhrase'" do
+      expect(response.body).to include("Badges")
+    end
+  end
+
+end
