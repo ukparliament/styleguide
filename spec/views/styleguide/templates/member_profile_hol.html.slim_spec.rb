e24a05be5af715b3b14c29878cee0e63fa4c9fd3
vinternet-pds <vinterj@parliament.uk>
2016-12-15 09:55:22 +0000

    API call & Rspec updates to HoC & HoL templates

--- spec/views/styleguide/templates/member_profile_hol.html.slim_spec.rb
+++ spec/views/styleguide/templates/member_profile_hol.html.slim_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "styleguide/templates/member_profile_hol.html.slim", type: :view do
 
-  describe "GET #a-z" do
+  describe "GET member_profile_hol" do
     before(:each) do
       # you'll need to specify both the template and the layout explicitly
       render template: 'styleguide/templates/member_profile_hol', layout: 'layouts/sub_layout'
